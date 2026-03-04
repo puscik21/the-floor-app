a49253d6824e39ed5d3d2f192ed47bc813729c78
Grzegorz Puscizna <grzesiekpuscizna@gmail.com>
2026-03-04 17:16:18 +0100

    Fix PMD issue

--- backend/src/main/java/com/thefloorapp/config/WebSocketConfig.java
+++ backend/src/main/java/com/thefloorapp/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.StompWebSocketEndpointRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
@@ -32,7 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        var registration = registry.addEndpoint("/ws");
+        StompWebSocketEndpointRegistration registration = registry.addEndpoint("/ws");
         if (appCorsProperties.allowedOrigins().length > 0) {
             registration.setAllowedOrigins(appCorsProperties.allowedOrigins());
         }
